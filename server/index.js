974ce0a68a7b632779f1b35fc0f41098f9650714
Mert Yılmaz <myyilmaz97@gmail.com>
2021-03-17 01:37:20 +0300

    final attempt

--- server/index.js
+++ server/index.js
@@ -6,10 +6,20 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+io.set('transports', ['websocket'])
 
+io.on("connection", (socket) => { 
+  // console.log("Backend, connected!"); // user or client is connected.
+  console.log("User connected with a unique socketId ", socket.id);
+  socket.emit('mert','connected');  
+  socket.on("msg", function (msg) {
+        console.log("entered!"); // <--- It will print now !
+        
+        console.log("message: " + msg);
+        
+    });
+
+});
 http.listen(3030, () => {
   console.log('listening on *:3030');
 });
